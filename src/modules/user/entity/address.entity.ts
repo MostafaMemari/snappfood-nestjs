8be889f0d88b9e28c7af1d22e413ce902f0cfb51
MostafaMemari <m.memari7@yahoo.com>
2024-08-09 17:10:03 +0330

    first commit

---
+++ src/modules/user/entity/address.entity.ts
@@ -0,0 +1,27 @@
+import { Column, CreateDateColumn, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { UserEntity } from './user.entity';
+import { EntityNames } from 'src/common/enum/entity-name.enum';
+
+@Entity(EntityNames.UserAddress)
+export class UserAddressEntity {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+  @Column()
+  title: string;
+  @Column()
+  province: string;
+  @Column()
+  city: string;
+  @Column()
+  address: string;
+  @Column({ nullable: true })
+  postal_code: string;
+  @Column()
+  userId: number;
+  @CreateDateColumn()
+  created_at: Date;
+  @ManyToOne(() => UserEntity, (user) => user.addressList, {
+    onDelete: 'CASCADE',
+  })
+  user: UserEntity;
+}
---
+++ src/modules/user/entity/user.entity.ts
@@ -0,0 +1,48 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { UserAddressEntity } from './address.entity';
+import { OTPEntity } from './otp.entity';
+import { EntityNames } from 'src/common/enum/entity-name.enum';
+
+@Entity(EntityNames.User)
+export class UserEntity {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+  @Column({ nullable: true })
+  first_name: string;
+  @Column({ nullable: true })
+  last_name: string;
+  @Column({ unique: true })
+  mobile: string;
+  @Column({ nullable: true, unique: true })
+  email: string;
+  @Column({ unique: true, nullable: true })
+  invite_code: string;
+  @Column({ default: 0 })
+  score: number;
+  @Column({ nullable: true })
+  agentId: number;
+  @Column({ nullable: true, default: false })
+  mobile_verify: boolean;
+  @CreateDateColumn({ type: 'time with time zone' })
+  created_at: Date;
+  @UpdateDateColumn({ type: 'time with time zone' })
+  updated_at: Date;
+
+  @OneToMany(() => UserAddressEntity, (address) => address.user)
+  addressList: UserAddressEntity[];
+
+  @Column({ nullable: true })
+  otpId: number;
+  @OneToOne(() => OTPEntity, (otp) => otp.user)
+  @JoinColumn()
+  otp: OTPEntity;
+}
